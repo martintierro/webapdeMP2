fe96a875944a18d54d237bff4e54ce63141739c6
martintierro <38676234+martintierro@users.noreply.github.com>
2019-08-05 15:36:17 +0800

    Added image upload

--- public/home.js
+++ public/home.js
@@ -24,6 +24,12 @@ $(document).ready(function(){
         on();
     })
 
+    document.getElementById('picbutton').addEventListener('click', openDialog);
+    //button now clicks file button
+    function openDialog() {
+        document.getElementById('fileid').click();
+    }
+
 
 
     function on() {
